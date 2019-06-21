871a8c4cabaace2dd6848d79e0de59bc5400a635
jameskomo <james.komoh@gmail.com>
2019-06-21 10:15:47 +0300

    API Authentication set up through REST tokens

--- time_management/urls.py
+++ time_management/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('timeapp/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+
+# REST IMPORTS
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +31,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('timeapp.urls')),
+    # REST AUTHENTICATION ROUTE
+    url(r'^api-token-auth/', obtain_auth_token)
 ]
 
 
