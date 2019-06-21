a267f381bd360f1a87447b27c6b463c4f4e6f409
jameskomo <james.komoh@gmail.com>
2019-06-21 21:19:13 +0300

    Issues resoved after AJAX integration

--- timeapp/tests.py
+++ timeapp/tests.py
@@ -1,30 +1,6 @@
 from django.test import TestCase
 import datetime as dt
-from .models import Post, Neighborhood,Business,Contact
-
-class NeighborhoodTestClass(TestCase):
-    # Set up method
-    def setUp(self):
-        self.neighborhood= Neighborhood(neighborhood_name = 'Red Ville', neighborhood_location ='Liam Street', occupants_count ='100', neighborhood_image='media/images/default.jpg')
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.neighborhood,Neighborhood))
-
-class BusinessTestClass(TestCase):
-    # Set up method
-    def setUp(self):
-        self.business= Business(business_name = 'Komo Enterprises', business_location ='Liam Street', business_email ='test@testing.com', business_description='Dealing in all things household', business_image='media/images/default.jpg')
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.business,Business))
-
-class ContactTestClass(TestCase):
-    # Set up method
-    def setUp(self):
-        self.contact= Contact(contact_name = 'Kenya Health', contact_address='Liam Street', contact_email ='test@testing.com', contact_logo='media/images/default.jpg')
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.contact,Contact))
+from .models import Post
 
 class PostTestClass(TestCase):
     # Set up method
@@ -34,13 +10,7 @@ class PostTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.post,Post))
 
-
-      
-
     def tearDown(self):
-        Neighborhood.objects.all().delete()
         Post.objects.all().delete()
-        Business.objects.all().delete()
-        Contact.objects.all().delete()
     
    
